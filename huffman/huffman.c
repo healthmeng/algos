0069698c4b39a60137d0a3fd83f14514fdadcd14
healthmeng <24625527@qq.com>
2021-02-20 13:50:40 +0800

    finished basic huffman code and test

--- huffman/huffman.c
+++ huffman/huffman.c
@@ -4,27 +4,57 @@
 
 #include "huffman.h"
 
-int compweight(const void* e1, const void* e2){
-		HFTree* t1=(HFTree*)e1;
-		HFTree* t2=(HFTree*)e2;
-		return t1->weight-t2->weight;
-}
 
 void SelectNode(HFTree* arr, int num)
 {
 	int i;
 	int min1, min2,n1,n2;
-	for(i=0;i<num*2-1;i++){
-		
+	int get;
+	for(i=0,get=0;get<2;i++){
+			if(i>=num){
+				printf("Impossible,there must be some errors!\n");
+				exit(1);
+			}
+			if(arr[i].parent==-1){
+					if(get==0){
+						min1=arr[i].weight;
+						n1=i;
+					}else{
+						min2=arr[i].weight;
+						n2=i;
+					}
+					get++;
+			}
 	}
+	if(min1>min2){
+		int temp;
+		SWAP(min1,min2);
+		SWAP(n1,n2);
+	}
+	for(/*i has the proper value*/;i<num;i++){
+		if(arr[i].parent==-1){
+			if(arr[i].weight<min2){
+				min2=arr[i].weight;
+				n2=i;
+			}
+			if(min1>min2){
+				SWAP(min1,min2);
+				SWAP(n1,n2);
+			}
+		}
+	}
+	arr[i].weight=min1+min2;
+	arr[i].lchild=n1;
+	arr[i].rchild=n2;
+	arr[n1].parent=i;
+	arr[n2].parent=i;
 }
 
 void CreateTree(HFTree* arr, int num)
 {
 	int i;
-	qsort(arr,num,sizeof(HFTree),compweight);
 	for(i=0;i<num-1;i++)
-			SelectNode(arr,num);
+			SelectNode(arr,num+i);
 }
 
 HFTree* InitTree(int num)
@@ -32,15 +62,101 @@ HFTree* InitTree(int num)
 	HFTree* root=(HFTree*)malloc(sizeof(HFNode)*(num*2-1));
 	int i;
 	for(i=0;i<num;i++)
+	{
 		memset(root+i,0,sizeof(HFNode));
+		root[i].parent=root[i].lchild=root[i].rchild=-1;
+	}
 	return root;
+}
+
+void GetData(HFTree* tree){
+	int i;
+/*	for(i=0;i<CHARS;i++){
+			printf("ascii:");
+			scanf("%c",&tree[i].data);
+			printf("times:");
+			scanf("%d",&tree[i].weight);
+	}*/
+	for(i=0;i<CHARS;i++)
+	{
+		tree[i].data='A'+i;
+		tree[i].weight=i*10+1;
+	}
+}
+
+void CreateCodes(HFTree* tree, int num, char codes[CHARS][CHARS])
+{
+	int i=0;
+	for(i=0;i<num;i++)
+	{
+		char temp[CHARS]={'\0'};
+		int start=num-1;
+		int cur;
+		for(cur=i;tree[cur].parent!=-1;cur=tree[cur].parent)
+		{
+			if(tree[tree[cur].parent].lchild==cur)
+				temp[start-1]='0';
+			else
+				temp[start-1]='1';
+			start--;
+		}
+		strcpy(codes[i],temp+start);
+	}
+}
+
+void PrtCodes(HFTree* tree,char codes[CHARS][CHARS],int num)
+{
+	int i;
+	for(i=0;i<num;i++)
+			printf("%c->%s\n",tree[i].data,codes[i]);
+}
+
+int FindCode(HFTree* tree,int num,const char* s){
+	int i=num*2-1-1;
+	int bit=0;
+	int n;
+	do{
+		if(s[bit]=='\0') break;
+		n=s[bit]-'0';
+		if(n!=0 && n!=1){
+			printf("error codes\n");
+			return 0;
+		}	
+		/* now we have got a new bit from s */
+		if(tree[i].lchild==-1) return 0;
+		if(n==0)
+			i=tree[i].lchild;
+		else i=tree[i].rchild;
+		bit++;
+	}while(s[bit]!='\0');
+	if(tree[i].lchild==-1) return tree[i].data;
+	else return 0;
+}
+
+void TestDecode(HFTree* tree, int num)
+{
+	char s[1024];
+	int ascii;
+	printf("Input decoded string:");
+	scanf("%s",s);
+	ascii=FindCode(tree,num,s);
+	if(ascii){
+		printf("Find origin char: %c\n",ascii);
+	}else{
+		printf("Invalid decoded string!\n");
+	}
 }
 
 int main()
 {
-	int num=5;
+	int num=CHARS;
 	int i;
-	HFTree* arr=InitTree(num);
-
+	char codes[CHARS][CHARS];
+	HFTree* arr=InitTree(num*2-1);
+	GetData(arr);
+	CreateTree(arr,num);
+	CreateCodes(arr,num,codes);
+	PrtCodes(arr,codes,num);
+	TestDecode(arr,num);
 	return 0;
 }
--- huffman/huffman.h
+++ huffman/huffman.h
@@ -1,6 +1,9 @@
 #ifndef __HUFFMAN_H__
 #define __HUFFMAN_H__
 
+#define SWAP(m1,m2) do{m1^=m2;m2^=m1;m1^=m2;}while(0)
+#define CHARS 5
+
 typedef struct _HFTree{
 	int data;
 	int weight;
@@ -11,5 +14,8 @@ HFTree* InitTree(int num);
 void CreateTree(HFTree* arr, int num);
 const char PrtCode(int data,char* code);
 int Decode(const char* code);
-
+void GetData(HFTree* arr);
+void CreateCodes(HFTree* arr,int num, char s[CHARS][CHARS]);
+int FindCode(HFTree* tree, int num, const char* str);
+void TestDecode(HFTree* tree, int num);
 #endif
